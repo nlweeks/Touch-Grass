c6dd560a690edfc254a552734c2790eacceb52b8
nlweeks <noah.weeks@gmail.com>
2025-04-11 13:15:49 -0400

    Initial Commit

---
+++ Touch Grass/Touch_GrassApp.swift
@@ -0,0 +1,17 @@
+//
+//  Touch_GrassApp.swift
+//  Touch Grass
+//
+//  Created by Noah Weeks on 4/11/25.
+//
+
+import SwiftUI
+
+@main
+struct Touch_GrassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
